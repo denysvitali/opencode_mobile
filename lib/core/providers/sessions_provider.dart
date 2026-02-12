31f409106dbc7f87a94030b6f7cb0ac70ced3eb1
Denys Vitali <denys@denv.it>
2026-02-12 18:38:41 +0000

    Fix Riverpod StateNotifier imports
    - Add state_notifier package dependency
    - Import StateNotifier from state_notifier package in all providers

--- lib/core/providers/sessions_provider.dart
+++ lib/core/providers/sessions_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:state_notifier/state_notifier.dart';
 
 import '../api/opencode_client.dart';
 import '../models/session.dart';
